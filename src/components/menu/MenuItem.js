5ad6ef92fd9f95e71271745212a5db09b76abae0
Sayan9836 <sayanmaity631@gmail.com>
2024-02-13 16:50:46 +0530

    fixes

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -3,8 +3,6 @@ import MenuItemTile from "@/components/menu/MenuItemTile";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useContext, useState } from "react";
-import FlyingButton from "react-flying-item";
-import toast from "react-hot-toast";
 
 export default function MenuItem(menuItem) {
   const { image, name, description, basePrice, sizes, extraIngredientPrices } =
@@ -119,14 +117,26 @@ export default function MenuItem(menuItem) {
                   ))}
                 </div>
               )}
-              <FlyingButton targetTop={"5%"} targetLeft={"95%"} src={image}>
-                <div
-                  className="primary sticky bottom-2"
-                  onClick={handleAddToCartButtonClick}
+              <button
+                onClick={handleAddToCartButtonClick}
+                class="flex m-0 w-full items-center justify-center rounded-[12px] bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="mr-2 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
                 >
-                  Add to cart ${selectedPrice}
-                </div>
-              </FlyingButton>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                Add to cart
+              </button>
               <button className="mt-2" onClick={() => setShowPopup(false)}>
                 Cancel
               </button>
